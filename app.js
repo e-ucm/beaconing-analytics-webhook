874fcd858b8678423598b3e7f2d0be8adcc7a909
Synpheros <Synpheros@hotmail.com>
2018-01-16 20:56:25 +0100

    Added Kafka queue usage in app root

--- app.js
+++ app.js
@@ -6,16 +6,58 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var config = require('./config');
-
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk(config.mongodb.uri);
 var index = require('./routes/index');
+var request = require('request');
 
 db.collection('clients');
 
 var app = express();
 app.config = config;
+
+// Initialize kafka and topic
+var queue = require('./lib/wrappers/kafka')();
+
+console.info(queue);
+
+queue.Init(app.config.kafka, function(error){
+  if(error){
+    console.log("Error on kafka inizialization");
+    return;
+  }
+
+  var webhookLib = require('./lib/webhook');
+  queue.CreateConsumer(app.config.kafka.topicName, function(message){
+    var value = JSON.parse(message.value);
+    var webhooks = [];
+
+    webhookLib.listWebhooks(db, {}, webhooks)(function(){
+      for(var i in webhooks){
+        this.options = {};
+        this.options.url = webhooks[i].payload_url;
+        this.options.method = "POST";
+        this.options.body = JSON.stringify(value.body);
+        this.options.headers = {
+          'Content-Type': 'application/json',
+          'X-Analytics-Event': value.event
+        }
+    
+        request(this.options, function(webhook){
+          return function(error, response, body){
+            if (!error && response.statusCode == 200) {
+              console.log("Notified " + webhook.name + "("+webhook.payload_url+") of event " + value.event);
+            }else
+              console.log("Can't notify " + webhook.name + "("+webhook.payload_url+") of event " + value.event);
+          }
+        }(webhooks[i]));
+      }
+    });
+  })
+});
+
+app.queue = queue;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
