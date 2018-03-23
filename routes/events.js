7c4503016dd81bc3670a335eb73f08efa26917c7
Synpheros <Synpheros@hotmail.com>
2018-03-23 17:37:39 +0100

    final check

--- routes/events.js
+++ routes/events.js
@@ -149,7 +149,7 @@ module.exports = function(auth, getBasePath, queue){
 				return res.json({message: 'Missing participants object'});
 	        }
 
-	        if(!checkArray(req.body.students, res) || !checkArray(req.body.teachers, res)){
+	        iif(!checkArray(req.body.participants.students, res) || !checkArray(req.body.participants.teachers, res)){
 				return;
 	        }
 
@@ -179,7 +179,7 @@ module.exports = function(auth, getBasePath, queue){
 				return res.json({message: 'Missing participants object'});
 	        }
 
-	        if(!checkArray(req.body.students, res) || !checkArray(req.body.teachers, res)){
+	        if(!checkArray(req.body.participants.students, res) || !checkArray(req.body.participants.teachers, res)){
 				return;
 	        }
 
